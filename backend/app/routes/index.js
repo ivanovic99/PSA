7bf5a2b7f1c51035c64f27c164b977a0485d3863
ivan <ivanerlich@gmail.com>
2023-07-04 01:51:39 -0300

    old backend deleted

--- backend/app/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ticketRoutes = require('./ticketRoutes');
-
-// Rutas para los tickets
-router.use('/tickets', ticketRoutes);
-
-module.exports = router;
