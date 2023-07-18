642cb18d72d3288b55a841e08f47259d6fb85ef8
ivan <ivanerlich@gmail.com>
2023-07-18 17:52:48 -0300

    hooks file added but not in use yet

---
+++ Backend/tests/hooks.js
@@ -0,0 +1,7 @@
+// hooks.js
+const { Before, BeforeAll } = require('@cucumber/cucumber');
+const Admin = require('../app/models/Admin'); // Adjust the path as needed
+
+Before(async function () {
+  
+});
