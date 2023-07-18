4545ad6576acff37d4030b6a759da9a6ed535bed
ivan <ivanerlich@gmail.com>
2023-07-18 19:56:48 -0300

    hooks were added and they are now in use (only for admin login at the moment)

--- Backend/tests/hooks.js
+++ Backend/tests/hooks.js
@@ -1,7 +1,33 @@
 // hooks.js
 const { Before, BeforeAll } = require('@cucumber/cucumber');
 const Admin = require('../app/models/Admin'); // Adjust the path as needed
+const axios = require('axios');
 
-Before(async function () {
-  
+BeforeAll({ tags: '@preLogin_createAdmin' }, async function () {
+   try {
+      await axios.post('http://localhost:5000/api/admin/signup', {
+         "email": "email@" + "preLogin_createAdmin" + ".com",
+     
+        "password": "123",
+     
+        "adminKey": "321",
+     
+        "username": "preLogin_createAdmin",
+        
+        "name": "{ type: String, required: false }",
+        
+        "lastname": "{ type: String, required: false }",
+        
+        "age": 10000,
+        
+        "nationality": "{ type: String, required: false }",
+        
+        "adress": "{ type: String, required: false }",
+        
+        "phone": "[{ type: String, required: false }]"
+     });
+     console.log("Admin created!");
+   } catch (error) {
+      console.log("error -------->", error);
+   }
 });
