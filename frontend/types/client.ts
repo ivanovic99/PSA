75001818f944641f0b97f73659856577a2b5dcca
ivan <ivanerlich@gmail.com>
2023-08-09 22:21:09 -0300

    client and user interface added

---
+++ frontend/types/client.ts
@@ -0,0 +1,16 @@
+export interface Client  {
+
+   CUIL: number,
+   
+   name: string,
+   
+   email: string,
+   
+   phone: number,
+   
+   country: string,
+   
+   city: string,
+   
+   address: string,
+}
